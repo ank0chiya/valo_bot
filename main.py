c308402f6d69a48aba8f6134273ee53c604d2152
ank <freedom.love.is.crazy@gmail.com>
2021-10-24 23:25:14 +0900

    add get skin function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import json
 
 
 client = discord.Client()
@@ -28,8 +29,26 @@ async def on_message(message):
 
         msg = await client.wait_for('message', check=check, timeout=10)
 
+        await message.channel.send(f"記入形式は｛武器種:武器名｝にしてください")
+
         await message.channel.send(f"Message {msg.content}")
         #await message.channel.send(f"{os.listdir(path='.')}")
+
+    if message.content == '/get_skin':
+        json_open = open('db/db.json', 'r')
+        json_load = json.load(json_open)
+
+        print(json_load['section1']['date'])
+
+        await message.channel.send(f"日付を入力してください ex)2020/1/1")
+        date_msg = await client.wait_for('message')
+
+        for date in json_load.values():
+            if date_msg == date["date"]:
+                await message.channel.send(date["wapon"])
+            else:
+                await message.channel.send("日付を確認してください")
+
 
 
 client.run(os.getenv('DISCORD_BOT_TOKEN'))
