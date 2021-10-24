bdb9aa27417ff04f946143a5fe69e7c1435421ed
ank <freedom.love.is.crazy@gmail.com>
2021-10-24 23:47:07 +0900

    add set wapon skin function

--- main.py
+++ main.py
@@ -27,24 +27,26 @@ async def on_message(message):
         def check(m):
             return m.author == message.author
 
-        msg = await client.wait_for('message', check=check, timeout=10)
+        await message.channel.send("今日の武器は？？？？")
+        await message.channel.send("記入形式は｛武器種:武器名｝にしてください")
+        wapon_list = []
+        for _ in range(4):
+            wapon_list.append(await client.wait_for('message', check=check, timeout=10))
+        
+        print(wapon_list)
 
-        await message.channel.send(f"記入形式は｛武器種:武器名｝にしてください")
-
-        await message.channel.send(f"Message {msg.content}")
+        await message.channel.send(f"Message {wapon_list[1].content}")
         #await message.channel.send(f"{os.listdir(path='.')}")
 
     if message.content == '/get_skin':
         json_open = open('db/db.json', 'r')
         json_load = json.load(json_open)
 
-        print(json_load['section1']['date'])
-
         await message.channel.send(f"日付を入力してください ex)2020/1/1")
         date_msg = await client.wait_for('message')
 
         for date in json_load.values():
-            if date_msg == date["date"]:
+            if date_msg.content == date["date"]:
                 await message.channel.send(date["wapon"])
             else:
                 await message.channel.send("日付を確認してください")
